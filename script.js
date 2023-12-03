b019994c1f4e1907064b727d1571d0debfbc5f82
ADorsey1 <152750517+ADorsey1@users.noreply.github.com>
2023-12-02 19:59:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function applyNow(jobTitle) {
+    alert(`Applying for ${jobTitle}!`);
+    
+}
+
+function submitApplication() {
+    alert('Application submitted successfully!');
+    
+}
+
+function navigateToJobs() {
+    window.location.href = 'job_openings.html';
+}
